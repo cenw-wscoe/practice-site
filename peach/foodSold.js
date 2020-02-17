6a2ea48186b9ec3ca59edaf497ecbf56f905fe0c
Andrew Nordlund <github@nordburg.ca>
2020-02-16 22:07:48 -0500

    WIP:Got table outputting properly

--- peach/foodSold.js
+++ peach/foodSold.js
@@ -38,46 +38,51 @@ function createTable (data) {
 				tbody=tbody[0];
 
 				for (var dte in data) {
-					var total = 0;
+					var total = 0, tottot =0;
 					var tr = document.createElement("tr");
+					tbody.append(tr);
 					var dateTD = document.createElement("td");
+					dateTD.setAttribute("rowspan", "3");
 					dateTD.innerHTML = dte;
 					tr.append(dateTD);
 					
 
 
 					if (data[dte].hasOwnProperty("beforeLunch")) {
-						var totBL, totAL, totSN, totTC, totC = 0;
-						dateTD.setAttribute("rowspan", "3");
+						var totBL =0, totAL=0, totSN=0, totTC=0, totC=0;
 						
-						var blTR = document.createElement("tr");
+						var blTH = document.createElement("th");
+						blTH.innerHTML = "Before Lunch";
+						tr.append(blTH);
 						
 						var blsnTD = document.createElement("td");
 						blsnTD.innerHTML = data[dte]["beforeLunch"]["sneezes"];
-						blTR.append(blsnTD);
+						tr.append(blsnTD);
 						totBL += data[dte]["beforeLunch"]["sneezes"];
 						totSN += data[dte]["beforeLunch"]["sneezes"];
 
 						var bltcTD = document.createElement("td");
 						bltcTD.innerHTML = data[dte]["beforeLunch"]["throatClears"];
-						blTR.append(bltcTD);
+						tr.append(bltcTD);
 						totBL += data[dte]["beforeLunch"]["throatClears"];
 						totTC += data[dte]["beforeLunch"]["throatClears"];
 
 						var cTD = document.createElement("td");
 						cTD.innerHTML = data[dte]["beforeLunch"]["coughs"];
-						blTR.append(cTD);
+						tr.append(cTD);
 						totBL += data[dte]["beforeLunch"]["coughs"];
 						totC += data[dte]["beforeLunch"]["coughs"];
 
 						var totalBLTD = document.createElement("td");
 						totalBLTD.innerHTML = totBL;
-						blTR.append(totalBLTD);
+						tr.append(totalBLTD);
 
-						tr.append(blTR);
-						
+						//tbody.append(blTR);
 
 						var alTR = document.createElement("tr");
+						var alTH = document.createElement("th");
+						alTH.innerHTML = "After Lunch";
+						alTR.append(alTH);
 						
 						var alsnTD = document.createElement("td");
 						alsnTD.innerHTML = data[dte]["afterLunch"]["sneezes"];
@@ -97,48 +102,108 @@ function createTable (data) {
 						totAL += data[dte]["afterLunch"]["coughs"];
 						totC += data[dte]["afterLunch"]["coughs"];
 
-						tr.append(alTR);
+						var totalALTD = document.createElement("td");
+						totalALTD.innerHTML = totAL;
+						alTR.append(totalALTD);
+
+						tbody.append(alTR);
 
 						var totTR = document.createElement("tr");
+						var totTH = document.createElement("th");
+						totTH.innerHTML = "Daily Total";
+						totTR.append(totTH);
+
+
 						var snTotTD = document.createElement("td");
 						snTotTD.innerHTML = totSN;
 						totTR.append(snTotTD);
+						tottot += totSN;
 
 						var tcTotTD = document.createElement("td");
 						tcTotTD.innerHTML = totTC;
 						totTR.append(tcTotTD);
+						tottot += totTC;
 
 						var cTotTD = document.createElement("td");
 						cTotTD.innerHTML = totC;
 						totTR.append(cTotTD);
-						
-						tr.append(totTR);
-						//tr.append(
+						tottot += totC;
 
+						var totalTD = document.createElement("td");
+						totalTD.innerHTML = tottot;
+						totTR.append(totalTD);
+						
+						tbody.append(totTR);
 						
 					} else {
-						var snTD = document.createElement("td");
-						snTD.innerHTML = data[dte]["sneezes"];
-						tr.append(snTD);
-						total += data[dte]["sneezes"];
+						var blTH = document.createElement("th");
+						blTH.innerHTML = "Before Lunch";
+						tr.append(blTH);
+						
+						var blsnTD = document.createElement("td");
+						blsnTD.innerHTML = "0";
+						tr.append(blsnTD);
+
+						var bltcTD = document.createElement("td");
+						bltcTD.innerHTML = "0";
+						tr.append(bltcTD);
+
+						var blcTD = document.createElement("td");
+						blcTD.innerHTML = "0";
+						tr.append(blcTD);
+
+						var totBLTD = document.createElement("td");
+						totBLTD.innerHTML = "0";
+						tr.append(totBLTD);
+
+						var alTR = document.createElement("tr");
+						var alTH = document.createElement("th");
+						alTH.innerHTML = "After Lunch";
+						alTR.append(alTH);
+
+						var alsnTD = document.createElement("td");
+						alsnTD.innerHTML = "0";
+						alTR.append(alsnTD);
+
+						var altcTD = document.createElement("td");
+						altcTD.innerHTML = "0";
+						alTR.append(altcTD);
+
+						var cTD = document.createElement("td");
+						cTD.innerHTML = "0";
+						alTR.append(cTD);
+
+						var totalALTD = document.createElement("td");
+						totalALTD.innerHTML = "0";
+						alTR.append(totalALTD);
+						tbody.append(alTR);
+
+						var totTR = document.createElement("tr");
+						var totTH = document.createElement("th");
+						totTH.innerHTML = "Daily Total";
+						totTR.append(totTH);
+
+						var snTotTD = document.createElement("td");
+						snTotTD.innerHTML = data[dte]["sneezes"];
+						totTR.append(snTotTD);
+						tottot += data[dte]["sneezes"];
 
 						var tcTD = document.createElement("td");
 						tcTD.innerHTML = data[dte]["throatClears"];
-						tr.append(tcTD);
-						total += data[dte]["throatClears"];
+						totTR.append(tcTD);
+						tottot += data[dte]["throatClears"];
 
 						var cTD = document.createElement("td");
 						cTD.innerHTML = data[dte]["coughs"];
-						tr.append(cTD);
-						total += data[dte]["coughs"];
+						totTR.append(cTD);
+						tottot += data[dte]["coughs"];
 
 						var totalTD = document.createElement("td");
-						totalTD.innerHTML = total;
-						tr.append(totalTD);
-
+						totalTD.innerHTML = tottot;
+						totTR.append(totalTD);
+						tbody.append(totTR);
 					}
 
-					tbody.append(tr);
 				}
 			} else {
 				console.log ("Didn't get tbody");
